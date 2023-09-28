5ddc840995eb2f50f9d5dcf4dc39d664556f736e
Meheer17 <meherr17.j@gmail.com>
2023-09-28 16:59:35 +0530

    DS Completed

--- Binary-Tree/main.c
+++ Binary-Tree/main.c
@@ -1,17 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_QUEUE_SIZE 100
 
-struct Node 
+typedef struct Node
 {
     int val;
     struct Node *left, *right;
-};
+} Node;
+
+typedef struct Queue
+{
+  Node *items[MAX_QUEUE_SIZE];
+  int front;
+  int rear;
+} Queue;
+
+void enqueue (Queue * q, Node * item)
+{
+  if (q->rear == MAX_QUEUE_SIZE - 1)
+    {
+      printf ("Queue Overflow\n");
+      exit (EXIT_FAILURE);
+    }
+  q->rear++;
+  q->items[q->rear] = item;
+}
+
+Node *dequeue (Queue * q)
+{
+  if (q->front == q->rear)
+    {
+      printf ("Queue Underflow\n");
+      exit (EXIT_FAILURE);
+    }
+  q->front++;
+  return q->items[q->front];
+}
+
+int is_empty (Queue * q)
+{
+  return q->front == q->rear;
+}
+
+void bfs (Node * root)
+{
+  Queue q;
+  q.front = -1;
+  q.rear = -1;
+
+  enqueue (&q, root);
+
+  while (!is_empty (&q))
+    {
+      Node *current = dequeue (&q);
+      printf ("%d ", current->val);
+
+      if (current->left != NULL)
+	{
+	  enqueue (&q, current->left);
+	}
+      if (current->right != NULL)
+	{
+	  enqueue (&q, current->right);
+	}
+    }
+}
 
 struct Node *create(int num)
 {
-    struct Node* new_node = (struct Node*) malloc (sizeof(struct Node));    
-    if(new_node == NULL)
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    if (new_node == NULL)
     {
         printf("\nMemory can't be allocated for new node\n");
         free(new_node);
@@ -25,82 +84,97 @@ struct Node *create(int num)
 
 struct Node *root = NULL;
 struct Node *current = NULL;
+struct Node *prev = NULL;
 
 void add(int num)
 {
     struct Node *new_n = create(num);
-    if(!root){
+    if (!root)
+    {
         root = new_n;
         return;
     }
     current = root;
-    while(current){
-        if(current->val == new_n->val){
+    while (current)
+    {
+        if (current->val == new_n->val)
+        {
             return;
         }
-        if(current->val > new_n->val){
-            if(!current->left){
+        if (current->val > new_n->val)
+        {
+            if (!current->left)
+            {
                 current->left = new_n;
                 return;
             }
             current = current->left;
-        } else {
-            if(!current->right){
+        }
+        else
+        {
+            if (!current->right)
+            {
                 current->right = new_n;
                 return;
             }
             current = current->right;
         }
-    } 
+    }
 }
 
 int Max()
 {
-    if(!root) return 0;
+    if (!root)
+        return 0;
     current = root;
-    while (current->right) current = current->right;
+    while (current->right)
+        current = current->right;
     return current->val;
 }
 
-int Min(){
-    if(!root) return 0;
+int Min()
+{
+    if (!root)
+        return 0;
     current = root;
-    while (current->left) current = current->left;
+    while (current->left)
+        current = current->left;
     return current->val;
 }
 
 int isPresent(int num)
 {
-    if(!root) return 0;
+    if (!root)
+        return 0;
     current = root;
-    while(current)
+    while (current)
     {
-        if(current->val == num)
+        if (current->val == num)
         {
             return 1;
-        } 
-        
-        else 
+        }
+
+        else
         {
-            if(current->val > num)
+            if (current->val > num)
             {
-                if(current->left == NULL)
+                if (current->left == NULL)
                 {
                     return 0;
-                } 
-                
-                else 
+                }
+
+                else
                 {
                     current = current->left;
                 }
-            } 
-            else 
+            }
+            else
             {
-                if(current->right == NULL)
+                if (current->right == NULL)
                 {
                     return 0;
-                } 
-                else 
+                }
+                else
                 {
                     current = current->right;
                 }
@@ -109,16 +183,10 @@ int isPresent(int num)
     }
 }
 
-// void isBinTree (struct Node *node){
-//     if(!node || ((!node->left || node->left->val < node->val) &&
-//         (!node->right || node->right->val >= node->val) &&
-//         subTree(node->left) && subTree(node->right))) return 1;
-//     return 0;
-// }
-
-void printInorder(struct Node* node)
+void printInorder(struct Node *node)
 {
-    if (node == NULL) return;
+    if (node == NULL)
+        return;
     printInorder(node->left);
     printf("%d ", node->val);
     printInorder(node->right);
@@ -126,7 +194,8 @@ void printInorder(struct Node* node)
 
 void printPreOrder(struct Node *node)
 {
-    if(node == NULL) return;
+    if (node == NULL)
+        return;
     printf("%d ", node->val);
     printPreOrder(node->left);
     printPreOrder(node->right);
@@ -134,10 +203,51 @@ void printPreOrder(struct Node *node)
 
 void printPostOrder(struct Node *node)
 {
-    if(node == NULL) return;
+    if (node == NULL)
+        return;
     printPostOrder(node->left);
     printPostOrder(node->right);
     printf("%d ", node->val);
+}
+
+void delete(int num){
+    if(!root) return NULL;
+    current = root;
+    prev = current;
+    while(current && current->val !== num){
+        prev = current;
+        if(current->val > num){
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+    if(!current) return NULL;
+    else {
+        int num_nodes = 0;
+        void prev_del(){
+            if(prev->left == current) prev->left = NULL;
+            else prev->right = NULL;
+        }
+        if(current->left && current->right) num_nodes = 2;
+        else if(current->left || current->right) num_nodes = 1;
+        if(!prev) root = NULL;
+        if(num_nodes == 0){
+            prev_del();
+        } else if(num_nodes == 1){
+            if(current->left){
+                prev_del();
+                add(current->left->val);
+            } else {
+                prev_del();
+                add(current->right->val);
+            }
+        } else if(num_nodes == 2){
+            prev_del()
+            add(current->left->val);
+            add(current->right->val);               
+        }
+    }
 }
 
 int main()
@@ -148,6 +258,6 @@ int main()
     add(9);
     add(8);
     add(1);
-    printPostOrder(root);
+    bfs(root);
     return 0;
 }
